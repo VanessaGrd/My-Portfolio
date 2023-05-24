33a80e2d7ce91877d0ca84641fb177636c6bc0ba
VanessaGrd <suede210810@gmail.com>
2023-05-24 18:33:50 +0200

    update overlayProjects

--- my-portfolio-app/src/components/Projects.jsx
+++ my-portfolio-app/src/components/Projects.jsx
@@ -14,31 +14,37 @@ function Projects() {
           <a href="https://wildcodeschool.github.io/2023-02-JS-FT-Lyon-P1-WeWildin/">
             <img src={projet1} alt="projet1" />
           </a>
+          <div className={styles.overlay}>
           <p>
             1er projet pédagogique réalisé en groupe lors de ma formation à la
             Wild Code School. <br></br>Portfolio des étudiants de la WCS à destination
             des recruteurs qui peuvent filtrer en fonction des hardskills et des
             softskills recherchés. Technos utilisées : HTML, CSS, JavaScript,
             Git/Github, Figma, Trello.
-          </p>
+  </p>
+  </div>
           </div>
           <div className={styles.project}>
           <a href="https://daily-expat.netlify.app/">
             <img src={projet2} alt="projet2" />
           </a>
-          <p>
+          <div className={styles.overlay}>
+
+         <p>
             2ème projet pédagogique réalisé en groupe lors de ma formation à la
             Wild Code School. <br></br>Site à destination des expatriés qui souhaitent
             renouer avec leur pays d'origine.<br></br> Ils peuvent retrouver des images,
             des musiques, des recettes et les news de leur pays. Technos
             utilisées : HTML, CSS, React, JavaScript, API, NodeJS, Git/Github,
             Figma, Trello.
-          </p>
+</p></div>
           </div>
           <div className={styles.project3}>
           <a href="">
             <img src={projet3} alt="projet3" />
           </a>
+          <div className={styles.overlayP3}>
+
           <p>
             Projet réalisé en groupe lors d'un hackathon de 24h. Site d'une
             association qui propose de réserver des visites gratuites de
@@ -46,7 +52,7 @@ function Projects() {
             volontaires disponibles. Technos utilisées : HTML, CSS, React,
             JavaScript, API, NodeJS, Git/Github, Figma, Trello.
           </p>
-        
+        </div>
         </div>
         </div>
       </div>
